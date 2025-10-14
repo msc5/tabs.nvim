5429f4205c70a4e61df4fe16a128fd05ec6d7725
Matthew Coleman <msc5.coleman@gmail.com>
2025-10-14 17:07:25 -0400

    Set default TablineCurrentTab bg color to 'none'

--- lua/tabs/colors.lua
+++ lua/tabs/colors.lua
@@ -67,7 +67,7 @@ local function setup_highlights(colors)
     hl('TablineSessionIcon', { fg = colors.fg.red, bg = 'none' })
     hl('TablineSession', { fg = colors.fg.orange, bg = 'none', bold = true })
     hl('TablineTab', { fg = colors.fg.grey, bg = 'none' })
-    hl('TablineCurrentTab', { fg = colors.fg.blue, bg = colors.bg.dark, bold = true })
+    hl('TablineCurrentTab', { fg = colors.fg.blue, bg = 'none', bold = true })
 end
 
 local function setup_colors()
