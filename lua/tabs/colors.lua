c0afb8e63ee10fc31bcdb4db9e8acf940e15f867
Matthew Coleman <msc5.coleman@gmail.com>
2025-12-13 15:22:15 -0500

    Claude scroll tabs

--- lua/tabs/colors.lua
+++ lua/tabs/colors.lua
@@ -32,6 +32,7 @@ local function gather_colors()
             cyan = 'Character',
             light_blue = 'Label',
             pink = 'Macro',
+            yellow = 'WarningMsg',
         },
         bg_highlights = {
             dark = 'Folded',
@@ -68,6 +69,7 @@ local function setup_highlights(colors)
     hl('TablineSession', { fg = colors.fg.orange, bg = 'none', bold = true })
     hl('TablineTab', { fg = colors.fg.grey, bg = 'none' })
     hl('TablineCurrentTab', { fg = colors.fg.blue, bg = colors.bg.dark, bold = true })
+    hl('TablineTabCounter', { fg = colors.fg.yellow, bg = 'none', bold = true })
 end
 
 local function setup_colors()
